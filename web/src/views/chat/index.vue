8fbfa9cd35c68f76c35a6ea8c3ae9877c29d65de
Hao Wu <wuhaoecho@gmail.com>
2023-03-20 19:02:24 +0800

    update

--- web/src/views/chat/index.vue
+++ web/src/views/chat/index.vue
@@ -43,7 +43,6 @@ const loading = ref<boolean>(false)
 const sliderValue = ref<number>(10)
 
 const throttledUpdate = debounce(async (newValue: number, oldValue: number) => {
-  console.log(`sliderValue updated from ${oldValue} to ${newValue}`)
   await setChatSessionMaxContextLength(uuid, newValue)
 }, 200)
 
@@ -508,12 +507,28 @@ onUnmounted(() => {
 
 <template>
   <div class="flex flex-col w-full h-full">
-    <HeaderComponent v-if="isMobile" :using-context="usingContext" @export="handleExport"
-      @toggle-using-context="toggleUsingContext" />
+    <HeaderComponent
+      v-if="isMobile" :using-context="usingContext" @export="handleExport"
+      @toggle-using-context="toggleUsingContext"
+    />
     <main class="flex-1 overflow-hidden">
+      <NModal v-model:show="showModal">
+        <NCard style="width: 600px" title="会话设置" :bordered="false" size="huge" role="dialog" aria-modal="true">
+          <!-- <template #header-extra>
+                          Oops!
+                        </template> -->
+          上下文数量, 默认10 (会话开始的两条+ 最近的8条)
+          <NSlider v-model:value="sliderValue" :min="1" :max="20" show-tooltip />
+          <!-- <template #footer>
+                          Footer
+                        </template> -->
+        </NCard>
+      </NModal>
       <div id="scrollRef" ref="scrollRef" class="h-full overflow-hidden overflow-y-auto">
-        <div id="image-wrapper" class="w-full max-w-screen-xl m-auto dark:bg-[#101014]"
-          :class="[isMobile ? 'p-2' : 'p-4']">
+        <div
+          id="image-wrapper" class="w-full max-w-screen-xl m-auto dark:bg-[#101014]"
+          :class="[isMobile ? 'p-2' : 'p-4']"
+        >
           <template v-if="!dataSources.length">
             <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
               <SvgIcon icon="ri:bubble-chart-fill" class="mr-2 text-3xl" />
@@ -522,9 +537,11 @@ onUnmounted(() => {
           </template>
           <template v-else>
             <div>
-              <Message v-for="(item, index) of dataSources" :key="index" :date-time="item.dateTime" :text="item.text"
+              <Message
+                v-for="(item, index) of dataSources" :key="index" :date-time="item.dateTime" :text="item.text"
                 :inversion="item.inversion" :error="item.error" :loading="item.loading" :index="index"
-                @regenerate="onRegenerate(index)" @delete="handleDelete(index)" @after-edit="handleAfterEdit" />
+                @regenerate="onRegenerate(index)" @delete="handleDelete(index)" @after-edit="handleAfterEdit"
+              />
               <div class="sticky bottom-0 left-0 flex justify-center">
                 <NButton v-if="loading" type="warning" @click="handleStop">
                   <template #icon>
@@ -556,22 +573,15 @@ onUnmounted(() => {
               <SvgIcon icon="ri:chat-history-line" />
             </span>
           </HoverButton>
-          <NModal v-model:show="showModal">
-            <NCard style="width: 600px" title="会话设置" :bordered="false" size="huge" role="dialog" aria-modal="true">
-              <!-- <template #header-extra>
-                          Oops!
-                        </template> -->
-              上下文数量, 默认10 (会话开始的两条+ 最近的8条)
-              <NSlider v-model:value="sliderValue" :min="1" :max="20" show-tooltip />
-              <!-- <template #footer>
-                          Footer
-                        </template> -->
-            </NCard>
-          </NModal>
-          <NInput id="message_textarea" v-model:value="prompt" data-testid="message_textarea" type="textarea"
-            :autosize="{ minRows: 1, maxRows: isMobile ? 4 : 8 }" :placeholder="placeholder" @keypress="handleEnter" />
-          <NButton id="send_message_button" data-testid="send_message_button" type="primary" :disabled="buttonDisabled"
-            @click="handleSubmit">
+
+          <NInput
+            id="message_textarea" v-model:value="prompt" data-testid="message_textarea" type="textarea"
+            :autosize="{ minRows: 1, maxRows: isMobile ? 4 : 8 }" :placeholder="placeholder" @keypress="handleEnter"
+          />
+          <NButton
+            id="send_message_button" data-testid="send_message_button" type="primary" :disabled="buttonDisabled"
+            @click="handleSubmit"
+          >
             <template #icon>
               <span class="dark:text-black">
                 <SvgIcon icon="ri:send-plane-fill" />
