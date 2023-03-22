7c50269861afd31d2252d4203b4d270aef407c3f
Hao Wu <wuhaoecho@gmail.com>
2023-03-22 17:09:22 +0800

    change test pass

--- e2e/tests/07_set_session_max_len.spec.ts
+++ e2e/tests/07_set_session_max_len.spec.ts
@@ -12,7 +12,7 @@ test.skip('test', async ({ page }) => {
         await page.getByTestId('email').click();
         await page.getByTestId('email').locator('input').fill(test_email);
         await page.getByTestId('password').locator('input').click();
-        await page.getByTestId('password').locator('input').fill('@WuHao5');
+        await page.getByTestId('password').locator('input').fill('‘@ThisIsATestPass5'');
         await page.getByTestId('signup').click();
 
         await page.waitForTimeout(1000);
