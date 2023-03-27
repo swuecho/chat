f9382f6a0ffc0598694c8036171461e423224a4b
Hao Wu <wuhaoecho@gmail.com>
2023-03-27 11:20:10 +0800

    admin ui changes (#40)
    * update
    
    * chaos
    
    * ui change
    
    * update

--- web/src/views/admin/index.vue
+++ web/src/views/admin/index.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
-import type { Component, Ref } from 'vue'
-import { h, reactive, ref } from 'vue'
-import { NIcon, NLayout, NLayoutSider, NMenu, NSpace } from 'naive-ui'
+import type { CSSProperties, Component, Ref } from 'vue'
+import { computed, h, reactive, ref } from 'vue'
+import { NIcon, NLayout, NLayoutSider, NMenu } from 'naive-ui'
 import type { MenuOption } from 'naive-ui'
 import { PulseOutline, ShieldCheckmarkOutline } from '@vicons/ionicons5'
 import { RouterLink } from 'vue-router'
 import i18n from '@/locales'
+import { useBasicLayout } from '@/hooks/useBasicLayout'
+import { HoverButton, SvgIcon } from '@/components/common'
+
+const { isMobile } = useBasicLayout()
+
+const collapsed: Ref<boolean> = ref(false)
+const activeKey: Ref<string | null> = ref(null)
+
+const getMobileClass = computed<CSSProperties>(() => {
+  if (isMobile.value) {
+    return {
+      position: 'fixed',
+      zIndex: 50,
+    }
+  }
+  return {}
+})
 
 function renderIcon(icon: Component) {
   return () => h(NIcon, null, { default: () => h(icon) })
@@ -42,27 +59,58 @@ const menuOptions: MenuOption[] = reactive([
   },
 ])
 
-const collapsed: Ref<boolean> = ref(false)
-const activeKey: Ref<string | null> = ref(null)
+function handleUpdateCollapsed() {
+  collapsed.value = !collapsed.value
+}
 </script>
 
 <template>
   <div>
-    <NSpace vertical>
-      <NLayout has-sider>
-        <NLayoutSider
-          bordered collapse-mode="width" :collapsed-width="64" :width="240" :collapsed="collapsed"
-          show-trigger @collapse="collapsed = true" @expand="collapsed = false"
+    <div class="h-full dark:bg-[#24272e] transition-all" :class="[isMobile ? 'p-0' : 'p-4']">
+      <div class="h-full overflow-hidden" :class="getMobileClass">
+        <header
+          class="sticky top-0 left-0 right-0 z-30 border-b dark:border-neutral-800 bg-white/80 dark:bg-black/20 backdrop-blur"
         >
-          <NMenu
-            v-model:value="activeKey" :collapsed="collapsed" :collapsed-width="64" :collapsed-icon-size="22"
-            :options="menuOptions"
-          />
-        </NLayoutSider>
-        <NLayout>
-          <router-view />
+          <div class="relative flex items-center justify-between min-w-0 overflow-hidden h-14">
+            <div class="flex items-center">
+              <button class="flex items-center justify-center w-11 h-11" @click="handleUpdateCollapsed">
+                <SvgIcon v-if="collapsed" class="text-2xl" icon="ri:align-justify" />
+                <SvgIcon v-else class="text-2xl" icon="ri:align-right" />
+              </button>
+            </div>
+            <h1 class="flex-1 px-4 pr-6 overflow-hidden cursor-pointer select-none text-ellipsis whitespace-nowrap">
+              管理后台
+            </h1>
+            <div class="flex items-center space-x-2">
+              <HoverButton>
+                <span class="text-xl">
+                  <SvgIcon icon="ri:chat-history-line" />
+                </span>
+              </HoverButton>
+              <HoverButton>
+                <span class="text-xl text-[#4f555e] dark:text-white">
+                  <SvgIcon icon="ri:download-2-line" />
+                </span>
+              </HoverButton>
+            </div>
+          </div>
+        </header>
+        <NLayout has-sider>
+          <NLayoutSider
+            bordered :width="240" :collapsed-width="0" :collapsed="collapsed" :show-trigger="isMobile ? false : 'arrow-circle'"
+            collapse-mode="transform" position="absolute" :style="getMobileClass"
+            @collapse="collapsed = true" @expand="collapsed = false"
+          >
+            <NMenu
+              v-model:value="activeKey" :collapsed="collapsed" :collapsed-width="64" :collapsed-icon-size="22"
+              :options="menuOptions"
+            />
+          </NLayoutSider>
+          <NLayout>
+            <router-view />
+          </NLayout>
         </NLayout>
-      </NLayout>
-    </NSpace>
+      </div>
+    </div>
   </div>
 </template>
