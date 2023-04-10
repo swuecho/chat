eb2ef0c93c1885142fc85125d505e180eec18fdb
Hao Wu <wuhaoecho@gmail.com>
2023-04-10 13:27:46 +0800

    update

--- web/src/views/chat/components/Session/SessionConfig.vue
+++ web/src/views/chat/components/Session/SessionConfig.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
+import type { Ref } from 'vue'
 import { computed, defineProps, ref, watch } from 'vue'
-import { NCard, NSlider, NSwitch } from 'naive-ui'
+import type { FormInst } from 'naive-ui'
+import { NCard, NForm, NFormItem, NSelect, NSlider, NSwitch } from 'naive-ui'
 import { debounce } from 'lodash-es'
 import { useChatStore } from '@/store'
 
@@ -12,78 +14,94 @@ const chatStore = useChatStore()
 
 const session = computed(() => chatStore.getChatSessionByUuid(props.uuid))
 
-const gpt_model = ref(session.value?.model === 'gpt-3.5-turbo')
-const slider = ref(session.value?.maxLength ?? 10)
-const temperature = ref(session.value?.temperature ?? 1.0)
-const maxTokens = ref(session.value?.maxTokens ?? 512)
-const topP = ref(session.value?.topP ?? 1.0)
-const debug = ref(session.value?.debug ?? false)
-// const frequencyPenalty = ref(0)
-// const presencePenalty = ref(0)
+interface ModelType {
+  gptModel: string
+  contextLength: number
+  temperature: number
+  maxTokens: number
+  topP: number
+  debug: boolean
+}
 
-const debouneUpdate = debounce(async ([newValueSlider, newValueTemperature, newMaxTokens, topP, debug, model]: Array<any>) => {
-  if (model)
-    model = 'gpt-3.5-turbo'
-  else
-    model = 'claude-v1'
+const modelRef: Ref<ModelType> = ref({
+  gptModel: session.value?.model ?? 'gpt-3.5-turbo',
+  contextLength: session.value?.maxLength ?? 10,
+  temperature: session.value?.temperature ?? 1.0,
+  maxTokens: session.value?.maxTokens ?? 512,
+  topP: session.value?.topP ?? 1.0,
+  debug: session.value?.debug ?? false,
+})
 
+const formRef = ref<FormInst | null>(null)
+
+const debouneUpdate = debounce(async (modelValue: Ref<ModelType>) => {
   chatStore.updateChatSession(props.uuid, {
-    maxLength: newValueSlider,
-    temperature: newValueTemperature,
-    maxTokens: newMaxTokens,
-    topP,
-    debug,
-    model,
+    maxLength: modelValue.value.contextLength,
+    temperature: modelValue.value.temperature,
+    maxTokens: modelValue.value.maxTokens,
+    topP: modelValue.value.topP,
+    debug: modelValue.value.debug,
+    model: modelValue.value.gptModel,
   })
 }, 200)
 
-watch([slider, temperature, maxTokens, topP, debug, gpt_model], ([newValueSlider, newValueTemperature, newMaxTokens, newTopP, newDebug, newGptModel], _) => {
-  debouneUpdate([newValueSlider, newValueTemperature, newMaxTokens, newTopP, newDebug, newGptModel])
+watch(modelRef, async (modelValue: Ref<ModelType>, oldValue: Ref<ModelType>) => {
+  console.log('watch')
+  console.log('modelValue', modelValue.value)
+  debouneUpdate(modelValue)
 })
+
+const modelOptions = [
+  {
+    label: 'gpt-3.5-turbo(chatgpt)',
+    value: 'gpt-3.5-turbo',
+  },
+  {
+    label: 'claude-v1 (claude)',
+    value: 'claude-v1',
+  },
+]
+
 </script>
 
 <template>
   <!-- https://platform.openai.com/playground?mode=chat -->
-  <NCard style="width: 600px" :title="$t('chat.sessionConfig')" :bordered="false" size="huge" role="dialog" aria-modal="true">
-    <div> {{ $t('chat.model') }}</div>
-    <!--
-    <NSelect v-model:value="gpt_model" :options="model_options" clearable />
-  -->
-    <NSwitch v-model:value="gpt_model">
-      <template #checked>
-        chatgpt
-      </template>
-      <template #unchecked>
-        claude
-      </template>
-    </NSwitch>
-    <div>{{ $t('chat.slider') }}: {{ slider }}</div>
-    <NSlider v-model:value="slider" :min="1" :max="20" :tooltip="false" />
-
-    <div>{{ $t('chat.temperature') }}: {{ temperature }}</div>
-    <NSlider v-model:value="temperature" :min="0.1" :max="1" :step="0.01" :tooltip="false" />
-
-    <div>{{ $t('chat.topP') }}: {{ topP }}</div>
-    <NSlider v-model:value="topP" :min="0" :max="1" :step="0.01" :tooltip="false" />
-
-    <div>{{ $t('chat.maxTokens') }}: {{ maxTokens }}</div>
-    <NSlider v-model:value="maxTokens" :min="256" :max="2048" :step="16" :tooltip="false" />
-    <div> {{ $t('chat.debug') }}</div>
-    <NSwitch v-model:value="debug">
-      <template #checked>
-        启用
-      </template>
-      <template #unchecked>
-        关闭
-      </template>
-    </NSwitch>
-    <!--
-                                          <div>{{ $t('chat.presencePenalty') }}</div>
-                                          <NSlider v-model:value="presencePenalty" :min="-2" :max="2" :step="0.1" :tooltip="false" />
-                                          <NInputNumber v-model:value="presencePenalty" size="small" />
-                                          <div>{{ $t('chat.frequencyPenalty') }}</div>
-                                          <NSlider v-model:value="frequencyPenalty" :min="-2" :max="2" :step="0.1" :tooltip="false" />
-                                          <NInputNumber v-model:value="frequencyPenalty" size="small" />
-                                           -->
+  <NCard id="session-config" :title="$t('chat.sessionConfig')" :bordered="false" size="medium">
+    <div>
+      <NForm ref="formRef" :model="modelRef" size="small" label-placement="left" :label-width="120">
+        <NFormItem :label="$t('chat.model')" path="gptModel">
+          <NSelect v-model:value="modelRef.gptModel" :options="modelOptions" />
+        </NFormItem>
+        <NFormItem :label="$t('chat.contextCount')" path="contextLength">
+          <NSlider v-model:value="modelRef.contextLength" :min="1" :max="20" :tooltip="false" show-tooltip />
+        </NFormItem>
+        <NFormItem :label="$t('chat.temperature')" path="temperature">
+          <NSlider v-model:value="modelRef.temperature" :min="0.1" :max="1" :step="0.01" :tooltip="false" />
+        </NFormItem>
+        <NFormItem :label="$t('chat.topP')" path="topP">
+          <NSlider v-model:value="modelRef.topP" :min="0" :max="1" :step="0.01" :tooltip="false" />
+        </NFormItem>
+        <NFormItem :label="$t('chat.maxTokens')" path="maxTokens">
+          <NSlider v-model:value="modelRef.maxTokens" :min="256" :max="2048" :step="16" :tooltip="false" />
+        </NFormItem>
+        <NFormItem :label="$t('chat.debug')" path="debug">
+          <NSwitch v-model:value="modelRef.debug">
+            <template #checked>
+              {{ $t('chat.enable_debug') }}
+            </template>
+            <template #unchecked>
+              {{ $t('chat.disable_debug') }}
+            </template>
+          </NSwitch>
+        </NFormItem>
+      </NForm>
+      <pre>{{ JSON.stringify(modelRef, null, 2) }} </pre>
+    </div>
   </NCard>
 </template>
+
+<style>
+#session-config {
+  width: 600px
+}
+</style>
