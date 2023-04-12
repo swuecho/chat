5fad11b4890e5cf4945a8e8c4edee7cf56410c45
Hao Wu <wuhaoecho@gmail.com>
2023-04-12 09:26:07 +0800

    more claude model

--- web/src/views/chat/components/Session/SessionConfig.vue
+++ web/src/views/chat/components/Session/SessionConfig.vue
@@ -60,6 +60,18 @@ const chatModelOptions = [
     value: 'claude-v1',
   },
   {
+    label: 'claude-instant(small,fast)',
+    value: 'claude-instant-v1',
+  },
+  {
+    label: 'claude-v1.2 (slightly better than v1)',
+    value: 'claude-v1.2',
+  },
+  {
+    label: 'claude-v1.3 (better than v1, code, non-english)',
+    value: 'claude-v1.3',
+  },
+  {
     label: 'gpt-4(chatgpt)',
     value: 'gpt-4',
   },
@@ -109,10 +121,10 @@ const chatModelOptions = [
         </NFormItem>
       </NForm>
       <!--
-                <div class="center">
-                  <pre>{{ JSON.stringify(modelRef, null, 2) }} </pre>
-                </div>
-                -->
+                    <div class="center">
+                      <pre>{{ JSON.stringify(modelRef, null, 2) }} </pre>
+                    </div>
+                    -->
     </div>
   </NCard>
 </template>
