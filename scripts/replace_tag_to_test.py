48c9172cefb97765e9282f2a4e3c58298be764c4
Hao Wu <wuhaoecho@gmail.com>
2023-04-12 13:05:43 +0800

    update

--- scripts/replace_tag_to_test.py
+++ scripts/replace_tag_to_test.py
@@ -5,10 +5,10 @@ parser.add_argument('tag')
 args = parser.parse_args()
 tag = args.tag
 
-with open('docker-compose.yml', 'r') as f:
+with open('docker-compose.yaml', 'r') as f:
     lines = f.readlines()
 
-with open('docker-compose.yml', 'w') as f:
+with open('docker-compose.yaml', 'w') as f:
     for line in lines:
         if 'latest' in line:
             line = line.replace('latest', tag)
