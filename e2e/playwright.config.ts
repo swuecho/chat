ab2236cae49aab2b073b9c502affbbcfbaf1a960
Hao Wu <wuhaoecho@gmail.com>
2023-04-14 13:47:17 +0800

    update

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
     // video: 'on', // Record videos of all tests to `test-results` folder (check `test-results/`).
     /* Base URL to use in actions like `await page.goto('/static/')`. */
     // baseURL: 'http://localhost:3000',
-    baseURL: process.env.CI ? "http://localhost:8080/#/chat" : "http://localhost:1002/#/chat",
+    baseURL: process.env.CI ? "http://localhost:8080/static/#/chat" : "http://localhost:1002/static/#/chat",
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
