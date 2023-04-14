95e28c9275e718fffcaa2190a9c2b34e25ee12b1
Hao Wu <wuhaoecho@gmail.com>
2023-04-14 19:07:10 +0800

    update

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -17,8 +17,8 @@ export default defineConfig((env) => {
       VitePWA({
         injectRegister: 'auto',
         manifest: {
-          name: 'chatGPT',
-          short_name: 'chatGPT',
+          name: 'Chat',
+          short_name: 'Chat',
           icons: [
             { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
             { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
