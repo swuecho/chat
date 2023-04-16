4353e27a74632218bbf240266f2b44806993191c
Hao Wu <wuhaoecho@gmail.com>
2023-04-16 13:07:20 +0800

    update

--- e2e/tests/chat_gpt_web.spec.ts
+++ e2e/tests/chat_gpt_web.spec.ts
@@ -1,5 +1,13 @@
 import { expect, test } from '@playwright/test'
 
+test('redirect to /static', async ({ page }) => {
+  await page.goto('/')
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Chat/)
+})
+
+
 test('has title', async ({ page }) => {
   await page.goto('/static/')
 
