12187e97bad577acab072b9f62aa08c6e98bbfad
Hao Wu <wuhaoecho@gmail.com>
2023-04-16 13:56:48 +0800

    update

--- api/sqlc/queries/auth_user_management.sql
+++ api/sqlc/queries/auth_user_management.sql
@@ -1,6 +1,7 @@
 -- name: GetRateLimit :one
 -- GetRateLimit retrieves the rate limit for a user from the auth_user_management table.
 -- If no rate limit is set for the user, it returns the default rate limit of 100.
-SELECT COALESCE(rate_limit, 100) AS rate_limit
+SELECT rate_limit AS rate_limit
 FROM auth_user_management
 WHERE user_id = $1;
+
