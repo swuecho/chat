69c5ffb1f2c91d6d7193f3a3a36e42233a716a86
Hao Wu <wuhaoecho@gmail.com>
2023-04-16 13:05:42 +0800

    update

--- e2e/tests/03_chat_session.spec.ts
+++ e2e/tests/03_chat_session.spec.ts
@@ -12,15 +12,14 @@ const pool = new Pool(db_config);
 
 
 test('test', async ({ page }) => {
-  await page.goto('/static/');
-
+  await page.goto('/');
   await page.getByTestId('email').click();
   await page.getByTestId('email').locator('input').fill(test_email);
   await page.getByTestId('password').locator('input').click();
   await page.getByTestId('password').locator('input').fill('@ThisIsATestPass5');
   await page.getByTestId('signup').click();
 
-
+  await page.waitForTimeout(500);
   await page.getByRole('complementary').getByRole('button').nth(1).click();
   await page.getByPlaceholder('请输入').dblclick();
   await page.getByPlaceholder('请输入').fill('This is a test topic');
