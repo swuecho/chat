114c9f83d09365f8d1359bce9dc74993e3f56a15
Hao Wu <wuhaoecho@gmail.com>
2023-04-18 16:32:19 +0800

    update

--- web/src/views/snapshot/components/Header/index.vue
+++ web/src/views/snapshot/components/Header/index.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { useRoute } from 'vue-router'
+import { ref } from 'vue'
 import { HoverButton, SvgIcon } from '@/components/common'
 import { updateChatSnapshot } from '@/api'
 
@@ -13,23 +14,46 @@ interface Props {
 
 const { uuid } = route.params as { uuid: string }
 
+const isEditing = ref<boolean>(false)
+
+const titleRef = ref(null)
+
 function handleHome() {
   window.open('#/snapshot_all', '_blank')
 }
 
 async function handleEdit(e: Event) {
   const title_value = (e.target as HTMLInputElement).innerText
+  isEditing.value = false
   await updateChatSnapshot(uuid, { title: title_value })
+}
+
+function handleEditTitle() {
+  isEditing.value = true
+  if (titleRef.value)
+    // @ts-expect-error focus is ok
+    titleRef.value.focus()
 }
 </script>
 
 <template>
   <header
-    class="sticky top-0 left-0 right-0 z-30 border-b dark:border-neutral-800 bg-white/80 dark:bg-black/20 backdrop-blur">
+    class="sticky top-0 left-0 right-0 z-30 border-b dark:border-neutral-800 bg-white/80 dark:bg-black/20 backdrop-blur"
+  >
     <div class="relative flex items-center justify-between min-w-0 overflow-hidden h-14">
-      <h1 class="flex-1 ml-5 px-4 pr-6 overflow-hidden  text-ellipsis whitespace-nowrap" contenteditable @blur="handleEdit">
+      <span class="ml-5">
+        <HoverButton :tooltip="$t('common.edit')" @click="handleEditTitle">
+          <SvgIcon icon="ic:baseline-edit" />
+        </HoverButton>
+      </span>
+      <h1
+        ref="titleRef" class="flex-1 overflow-hidden text-ellipsis whitespace-nowrap"
+        :class="[isEditing ? 'shadow-green-100' : '']" :contenteditable="isEditing" @blur="handleEdit"
+        @dblclick="handleEditTitle"
+      >
         {{ title ?? '' }}
       </h1>
+
       <div class="flex mr-5 items-center space-x-2">
         <HoverButton @click="handleHome">
           <span class="text-xl text-[#4f555e] dark:text-white">
@@ -49,6 +73,6 @@ h1[contenteditable] {
 
 h1[contenteditable]:focus {
   outline: none;
-  box-shadow: 0 0 0 2px #01bc77;
+  box-shadow: 0 0 0 2px #0be293;
 }
 </style>
