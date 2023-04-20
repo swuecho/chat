0adf473eafe288108c4370802b6d5fa9213c0bbe
Hao Wu <wuhaoecho@gmail.com>
2023-04-20 09:26:43 +0800

    update

--- web/src/views/snapshot/all.vue
+++ web/src/views/snapshot/all.vue
@@ -30,39 +30,30 @@ function post_url(uuid: string): string {
   <div class="flex flex-col w-full h-full">
     <header class="flex items-center justify-between p-4">
       <div class="flex items-center">
-        <svg
-          class="w-8 h-8 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-            d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-          />
+        <svg class="w-8 h-8 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
         </svg>
         <h1 class="text-2xl font-semibold text-gray-900">
           {{ $t('chat_snapshot.title') }}
         </h1>
       </div>
     </header>
-    <main class="flex-1 overflow-hidden">
+    <div id="scrollRef" ref="scrollRef" class="h-full overflow-hidden overflow-y-auto">
       <div class="max-w-screen-xl px-4 py-8 mx-auto">
         <ul class="space-y-4">
           <li v-for="post in posts" :key="post.uuid" class="flex justify-between">
             <div>
-              <time
-                :datetime="post.date"
-                class="block mb-2 text-sm font-medium text-gray-600"
-              >{{
+              <time :datetime="post.date" class="block mb-2 text-sm font-medium text-gray-600">{{
                 post.date }}</time>
-              <a
-                :href="post_url(post.uuid)" :title="post.title"
-                class="block text-xl font-semibold text-gray-900 hover:text-blue-600"
-              >{{
-                post.title }}</a>
+              <a :href="post_url(post.uuid)" :title="post.title"
+                class="block text-xl font-semibold text-gray-900 hover:text-blue-600">{{
+                  post.title }}</a>
             </div>
           </li>
         </ul>
       </div>
-    </main>
+    </div>
   </div>
 </template>
