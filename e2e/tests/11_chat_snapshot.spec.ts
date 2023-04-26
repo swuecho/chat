8a4c0345524bf1cd633dd93003a7d73c174624cd
Hao Wu <wuhaoecho@gmail.com>
2023-04-26 18:30:10 +0800

    update

--- e2e/tests/11_chat_snapshot.spec.ts
+++ e2e/tests/11_chat_snapshot.spec.ts
@@ -57,12 +57,16 @@ test('test', async ({ page }) => {
   await page_snapshot.waitForTimeout(500)
   snapshot_url = page_snapshot.url()
   expect(snapshot_url).toMatch(/snapshot/)
+  const message_counts_in_snapshot = await page_snapshot.$$eval('.message-text', (messages) => messages.length);
+  expect(message_counts_in_snapshot).toBe(4);
   // continue to chat
   await page_snapshot.locator('.floating-button > div > .flex').click();
   const page_back_to_chat_promise = page_snapshot.waitForEvent('popup');
   const page_back = await page_back_to_chat_promise
   expect(page_back.url()).toMatch(/chat/)
   await page_back.waitForTimeout(500)
+  const message_counts = await page_back.$$eval('.message-text', (messages) => messages.length);
+  expect(message_counts).toBe(4);
   const sessions_new = await selectChatSessionsByUserId(pool, user.id);
   // two conversion now
   expect(sessions_new.length).toBe(2)
