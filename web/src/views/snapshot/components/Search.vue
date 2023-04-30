a1ce7a061f109a7121a610766ed65f382cf35364
Hao Wu <wuhaoecho@gmail.com>
2023-04-30 17:13:50 +0800

    update (#272)

--- web/src/views/snapshot/components/Search.vue
+++ web/src/views/snapshot/components/Search.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { NInput, NList, NListItem } from 'naive-ui'
+import { debounce } from 'lodash-es'
 import { chatSnapshotSearch } from '@/api'
 
 interface SearchRecord {
@@ -15,10 +16,12 @@ const results = ref<SearchRecord[]>([])
 const search = async () => {
   results.value = await chatSnapshotSearch(searchText.value)
 }
+
+const debouncedSearch = debounce(search, 200)
 </script>
 
 <template>
-  <NInput v-model:value="searchText" placeholder="Search ...(support english only)" @keyup="search" />
+  <NInput v-model:value="searchText" placeholder="Search ...(support english only)" @keyup="debouncedSearch" />
   <NList>
     <NListItem v-for="result in results" :key="result.Uuid">
       <a :href="`/static/#/snapshot/${result.Uuid}`" target="_blank">{{ result.Title }}</a>
