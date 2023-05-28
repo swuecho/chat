788dcb5363a432876e7ee77b874019a4387549b7
Hao Wu <wuhaoecho@gmail.com>
2023-05-28 11:46:11 +0800

    add default prompts (#321)

--- web/src/views/chat/index.vue
+++ web/src/views/chat/index.vue
@@ -45,6 +45,7 @@ const showModal = ref<boolean>(false)
 
 // 添加PromptStore
 const promptStore = usePromptStore()
+
 // 使用storeToRefs，保证store修改后，联想部分能够重新渲染
 const { promptList: promptTemplate } = storeToRefs<any>(promptStore)
 
@@ -460,6 +461,8 @@ const footerClass = computed(() => {
 
 onMounted(() => {
   scrollToBottom()
+  // init default prompts
+  promptStore.getPromptList()
 })
 
 onUnmounted(() => {
