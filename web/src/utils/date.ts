9c0f616b11bb19638ae77714774805c6666428d2
Hao Wu <wuhaoecho@gmail.com>
2023-06-04 22:56:46 +0800

    fix_format_in_ios (#338)

--- web/src/utils/date.ts
+++ web/src/utils/date.ts
@@ -8,7 +8,7 @@ export function getCurrentDate() {
 
 export function displayLocaleDate(ts: string) {
   // const timestampFromDb = "2023-04-16 11:10:48.000"; // assume this is the value retrieved from the database
-  const dateObj = new Date(ts)
+  const dateObj = new Date(ts.replaceAll('-', '/'))
 
   const dateString = `${dateObj.toLocaleDateString()} ${dateObj.toLocaleTimeString()}`
 
