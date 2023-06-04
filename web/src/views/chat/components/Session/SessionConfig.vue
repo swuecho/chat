834fbf00219e10904047bac140d063e8fe8e80af
Hao Wu <wuhaoecho@gmail.com>
2023-06-04 19:51:30 +0800

    React query (#337)
    * add packages
    
    * query works
    
    * update
    
    * update

--- web/src/views/chat/components/Session/SessionConfig.vue
+++ web/src/views/chat/components/Session/SessionConfig.vue
@@ -1,15 +1,34 @@
 <script lang="ts" setup>
 import type { Ref } from 'vue'
-import { computed, onMounted, reactive, ref, watch } from 'vue'
+import { computed, ref, watch } from 'vue'
 import type { FormInst } from 'naive-ui'
-import { NForm, NFormItem, NRadio, NRadioGroup, NSlider, NSpace, NSwitch } from 'naive-ui'
+import { NForm, NFormItem, NRadio, NRadioGroup, NSlider, NSpace, NSpin, NSwitch } from 'naive-ui'
 import { debounce } from 'lodash-es'
 import { useChatStore } from '@/store'
 import { fetchChatModel } from '@/api'
 
+import {  useQuery } from "@tanstack/vue-query";
+
+const optiomFromModel = (model: any) => {
+  return {
+    label: model.label,
+    value: model.name,
+  }
+}
 const props = defineProps<{
   uuid: string
 }>()
+
+
+const { data, isLoading } = useQuery({
+  queryKey: ['chat_models'],
+  queryFn: fetchChatModel,
+  staleTime: 10 * 60 * 1000,
+})
+
+const chatModelOptions = computed(() => 
+  data?.value ? data.value.map(optiomFromModel) : []
+)
 
 const chatStore = useChatStore()
 
@@ -54,17 +73,7 @@ watch(modelRef, async (modelValue: ModelType) => {
   debouneUpdate(modelValue)
 }, { deep: true })
 
-const chatModelOptions: any[] = reactive([])
 
-onMounted(async () => {
-  const models = await fetchChatModel()
-  chatModelOptions.push(...models.map((model: any) => {
-    return {
-      label: model.label,
-      value: model.name,
-    }
-  }))
-})
 
 const tokenUpperLimit = computed(() => {
   if (modelRef.value.chatModel === 'gpt-4')
@@ -88,6 +97,7 @@ const tokenUpperLimit = computed(() => {
   <div>
     <NForm ref="formRef" :model="modelRef" size="small" label-placement="top" :label-width="20">
       <NFormItem :label="$t('chat.model')" path="chatModel">
+        <div v-if="isLoading"><NSpin size="medium" /></div>
         <NRadioGroup v-model:value="modelRef.chatModel">
           <NSpace>
             <NRadio v-for="model in chatModelOptions" :key="model.value" :value="model.value">
@@ -108,7 +118,8 @@ const tokenUpperLimit = computed(() => {
       <NFormItem :label="$t('chat.maxTokens', { maxTokens: modelRef.maxTokens })" path="maxTokens">
         <NSlider v-model:value="modelRef.maxTokens" :min="256" :max="tokenUpperLimit" :step="16" :tooltip="false" />
       </NFormItem>
-      <NFormItem v-if="modelRef.chatModel.startsWith('gpt') || modelRef.chatModel.includes('davinci')" :label="$t('chat.N', { n: modelRef.n })" path="n">
+      <NFormItem v-if="modelRef.chatModel.startsWith('gpt') || modelRef.chatModel.includes('davinci')"
+        :label="$t('chat.N', { n: modelRef.n })" path="n">
         <NSlider v-model:value="modelRef.n" :min="1" :max="10" :step="1" :tooltip="false" />
       </NFormItem>
       <NFormItem :label="$t('chat.debug')" path="debug">
@@ -123,9 +134,9 @@ const tokenUpperLimit = computed(() => {
       </NFormItem>
     </NForm>
     <!--
-                              <div class="center">
-                                <pre>{{ JSON.stringify(modelRef, null, 2) }} </pre>
-                              </div>
-                              -->
+                                        <div class="center">
+                                          <pre>{{ JSON.stringify(modelRef, null, 2) }} </pre>
+                                        </div>
+                                        -->
   </div>
 </template>
