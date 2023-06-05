884033a5c2466c1a75012ddd48548a94792ef808
Hao Wu <wuhaoecho@gmail.com>
2023-06-05 22:36:31 +0800

    update

--- web/src/utils/date.ts
+++ web/src/utils/date.ts
@@ -1,16 +1,17 @@
-import moment from 'moment'
+import { DateTime } from 'luxon';
+
 
 export function getCurrentDate() {
-  const now = moment()
-  const formattedDate = now.format('YYYY-MM-DD HH:mm:ss')
+  const now = DateTime.now()
+  const formattedDate = now.toFormat('YYYY-MM-DD HH:mm:ss')
   return formattedDate
 }
 
 export function displayLocaleDate(ts: string) {
   // const timestampFromDb = "2023-04-16 11:10:48.000"; // assume this is the value retrieved from the database
-  const dateObj = moment(ts)
+  const dateObj = DateTime.fromISO(ts)
 
-  const dateString = `${dateObj.format('LLL')}`
+  const dateString = dateObj.toFormat('D t')
 
   return dateString
 }
