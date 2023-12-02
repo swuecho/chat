1ba2a41c2fc50b6e9c70931870d3587d1734ee20
Hao Wu <wuhaoecho@gmail.com>
2023-12-02 20:09:49 +0800

    ollam-neural-chat

--- api/ollama.go
+++ api/ollama.go
@@ -20,3 +20,27 @@ func formatMinstralPrompt(chat_compeletion_messages []Message) string {
 	print(prompt)
 	return prompt
 }
+
+//### Prompt Format
+// ### System:
+// {system}
+// ### User:
+// {usr}
+// ### Assistant:
+func formatNeuralChatPrompt(chat_compeletion_messages []Message) string {
+	var sb strings.Builder
+
+	for _, message := range chat_compeletion_messages {
+		if message.Role == "system" {
+			sb.WriteString(fmt.Sprintf("### System:\n%s\n", message.Content))
+		} else if message.Role == "user" {
+			sb.WriteString(fmt.Sprintf("### User:\n%s\n", message.Content))
+		} else {
+			sb.WriteString(fmt.Sprintf("### Assistant:\n%s\n", message.Content))
+		}
+	}
+	sb.WriteString("### Assitant:\n")
+	prompt := sb.String()
+	print(prompt)
+	return prompt
+}
