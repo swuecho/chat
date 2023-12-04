c61f789401c58bb543c3492adeaed85551e9f4d2
Hao Wu <wuhaoecho@gmail.com>
2023-12-04 21:58:43 +0800

    update

--- api/ollama.go
+++ api/ollama.go
@@ -44,3 +44,26 @@ func formatNeuralChatPrompt(chat_compeletion_messages []Message) string {
 	print(prompt)
 	return prompt
 }
+
+// <|im_start|>system
+// {system}<|im_end|>
+// <|im_start|>user
+// {user}<|im_end|>
+// <|im_start|>assistant
+// {asistant}<|im_end|>
+func formatOpenHermesNeuralChatPrompt(chat_compeletion_messages []Message) string {
+	var sb strings.Builder
+
+	for _, message := range chat_compeletion_messages {
+		if message.Role == "system" {
+			sb.WriteString(fmt.Sprintf("<|im_start|>system\n%s<|im_end|>\n", message.Content))
+		} else if message.Role == "user" {
+			sb.WriteString(fmt.Sprintf("<|im_start|>user\n%s<|im_end|>\n", message.Content))
+		} else {
+			sb.WriteString(fmt.Sprintf("<|im_start|>assistant\n%s<|im_end|>\n", message.Content))
+		}
+	}
+	prompt := sb.String()
+	print(prompt)
+	return prompt
+}
