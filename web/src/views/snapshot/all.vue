53cce8c9d8dc60b5ffbd7ee21a758aa35f4431f7
Hao Wu <wuhaoecho@gmail.com>
2023-12-31 10:06:01 +0800

    display model in snapshot

--- web/src/views/snapshot/all.vue
+++ web/src/views/snapshot/all.vue
@@ -106,7 +106,7 @@ function handleDelete(post: PostLink) {
       <div class="max-w-screen-xl px-4 py-8 mx-auto">
         <div v-for="[yearMonth, postsOfYearMonth] in Object.entries(postsByYearMonth)" :key="yearMonth" class="flex mb-2">
           <h2 class="flex-none w-28 text-lg font-medium">
-            {{ yearMonth }}
+            {{ yearMonth }} 
           </h2>
           <ul>
             <li v-for="post in postsOfYearMonth" :key="post.uuid" class="flex justify-between">
--- web/src/views/snapshot/components/Message/index.vue
+++ web/src/views/snapshot/components/Message/index.vue
@@ -61,7 +61,7 @@ const code = computed(() => {
     </div>
     <div class="overflow-hidden text-sm " :class="[inversion ? 'items-end' : 'items-start']">
       <p class="text-xs text-[#b4bbc4]" :class="[inversion ? 'text-right' : 'text-left']">
-        {{ displayLocaleDate(dateTime) }}
+        {{ displayLocaleDate(dateTime) }} {{ model }}
       </p>
       <div class="flex items-end gap-1 mt-2" :class="[inversion ? 'flex-row-reverse' : 'flex-row']">
         <TextComponent
