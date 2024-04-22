88b195cf8316a3bd5eb1e9009b4389b492d856e5
Hao Wu <wuhaoecho@gmail.com>
2024-04-22 20:55:19 +0800

    fmt

--- web/src/views/chat/components/Message/index.vue
+++ web/src/views/chat/components/Message/index.vue
@@ -93,41 +93,41 @@ function handleRegenerate() {
 
 <template>
   <div class="chat-message">
-  <p class="text-xs text-[#b4bbc4] text-center">{{ displayLocaleDate(dateTime) }}</p>
-  <div class="flex w-full mb-6 overflow-hidden" :class="[{ 'flex-row-reverse': inversion }]">
-    <div class="flex items-center justify-center flex-shrink-0 h-8 overflow-hidden rounded-full basis-8"
-      :class="[inversion ? 'ml-2' : 'mr-2']">
-      <AvatarComponent :inversion="inversion" :model="model" />
-    </div>
-    <div class="overflow-hidden text-sm " :class="[inversion ? 'items-end' : 'items-start']">
-      <p :class="[inversion ? 'text-right' : 'text-left']">
-        {{ !inversion ? model : userInfo.name || $t('setting.defaultName') }}
-      </p>
-      <div class="flex items-end gap-1 mt-2" :class="[inversion ? 'flex-row-reverse' : 'flex-row']">
-        <TextComponent ref="textRef" class="message-text" :inversion="inversion" :error="error" :text="text"
-          :code="code" :loading="loading" :contenteditable="editable" :idex="index"
-          @blur="event => onContentChange(event, index)" />
-        <div class="flex flex-col">
-          <!-- testid="chat-message-regenerate" not ok, someting like testclass -->
-          <button v-if="!inversion"
-            class="chat-message-regenerate mb-2 transition text-neutral-300 hover:text-neutral-800 dark:hover:text-neutral-300"
-            @click="handleRegenerate">
-            <SvgIcon icon="ri:restart-line" />
-          </button>
-          <button v-if="!isPrompt && inversion"
-            class="mb-2 transition text-neutral-300 hover:text-neutral-800 dark:hover:text-neutral-300" :disabled="pining"
-            @click="emit('togglePin')">
-            <SvgIcon :icon="isPin ? 'ri:unpin-line' : 'ri:pushpin-line'" />
-          </button>
-          <NDropdown :placement="!inversion ? 'right' : 'left'" :options="options" @select="handleSelect">
-            <button class="transition text-neutral-300 hover:text-neutral-800 dark:hover:text-neutral-200">
-              <SvgIcon icon="ri:more-2-fill" />
-            </button>
-          </NDropdown>
-        </div>
+    <p class="text-xs text-[#b4bbc4] text-center">{{ displayLocaleDate(dateTime) }}</p>
+    <div class="flex w-full mb-6 overflow-hidden" :class="[{ 'flex-row-reverse': inversion }]">
+      <div class="flex items-center justify-center flex-shrink-0 h-8 overflow-hidden rounded-full basis-8"
+        :class="[inversion ? 'ml-2' : 'mr-2']">
+        <AvatarComponent :inversion="inversion" :model="model" />
       </div>
-      <AudioPlayer :text="text || ''" :right="inversion"></AudioPlayer>
+      <div class="overflow-hidden text-sm " :class="[inversion ? 'items-end' : 'items-start']">
+        <p :class="[inversion ? 'text-right' : 'text-left']">
+          {{ !inversion ? model : userInfo.name || $t('setting.defaultName') }}
+        </p>
+        <div class="flex items-end gap-1 mt-2" :class="[inversion ? 'flex-row-reverse' : 'flex-row']">
+          <TextComponent ref="textRef" class="message-text" :inversion="inversion" :error="error" :text="text"
+            :code="code" :loading="loading" :contenteditable="editable" :idex="index"
+            @blur="event => onContentChange(event, index)" />
+          <div class="flex flex-col">
+            <!-- testid="chat-message-regenerate" not ok, someting like testclass -->
+            <button v-if="!inversion"
+              class="chat-message-regenerate mb-2 transition text-neutral-300 hover:text-neutral-800 dark:hover:text-neutral-300"
+              @click="handleRegenerate">
+              <SvgIcon icon="ri:restart-line" />
+            </button>
+            <button v-if="!isPrompt && inversion"
+              class="mb-2 transition text-neutral-300 hover:text-neutral-800 dark:hover:text-neutral-300"
+              :disabled="pining" @click="emit('togglePin')">
+              <SvgIcon :icon="isPin ? 'ri:unpin-line' : 'ri:pushpin-line'" />
+            </button>
+            <NDropdown :placement="!inversion ? 'right' : 'left'" :options="options" @select="handleSelect">
+              <button class="transition text-neutral-300 hover:text-neutral-800 dark:hover:text-neutral-200">
+                <SvgIcon icon="ri:more-2-fill" />
+              </button>
+            </NDropdown>
+          </div>
+        </div>
+        <AudioPlayer :text="text || ''" :right="inversion"></AudioPlayer>
+      </div>
     </div>
-  </div>
   </div>
 </template>
