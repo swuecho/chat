a98994cf3cba1575c88a40327a647f2512cdabe9
Hao Wu <wuhaoecho@gmail.com>
2024-06-08 16:05:20 +0800

    update

--- web/src/views/chat/components/UploadModal.vue
+++ web/src/views/chat/components/UploadModal.vue
@@ -4,7 +4,7 @@
                         <NCard style="width: 600px" title="Upload" :bordered="false" size="huge" role="dialog"
                                 aria-modal="true">
                                 <template #header-extra>
-                                        upload doc, image, audio, video (gemmi flash only)
+                                        upload doc, image, audio, video 
                                 </template>
                                 <Uploader :sessionUuid="sessionUuid" :showUploaderButton="true"></Uploader>
                                 <template #footer>
