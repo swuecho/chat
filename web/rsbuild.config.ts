ccbd30fbd31524a4a307681f7f6ec494b5b89267
Hao Wu <wuhaoecho@gmail.com>
2024-08-28 21:13:01 +0800

    Refactor rsbuild.config.ts: Simplify configuration and remove unused options.

--- web/rsbuild.config.ts
+++ web/rsbuild.config.ts
@@ -3,9 +3,7 @@ import { defineConfig } from '@rsbuild/core';
 import {pluginVue } from '@rsbuild/plugin-vue';
 import { pluginLess} from "@rsbuild/plugin-less";
 
-export default defineConfig((env) => {
-  console.log(env);
-  return {
+export default defineConfig({
     html: {
       template: './index.html',
     },
@@ -14,19 +12,10 @@ export default defineConfig((env) => {
         index: './src/main.ts',
       },
     },
-    output: {
-      path: 'dist',
-      filename: '[name].js',
-    },
     plugins: [
       pluginVue(),
       pluginLess(),
     ],
-    resolve: {
-      alias: {
-        '@': path.resolve(process.cwd(), 'src'),
-      },
-    },
     server: {
       host: '0.0.0.0',
       port: 1002,
@@ -38,5 +27,4 @@ export default defineConfig((env) => {
         },
       },
     },
-  };
 });
