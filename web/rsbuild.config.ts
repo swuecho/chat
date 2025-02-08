93ba8edf86650decc3b24c818b9dda8bcfe45f98
Hao Wu <echowuhao@gmail.com>
2025-02-08 17:27:57 +0800

    more

--- web/rsbuild.config.ts
+++ web/rsbuild.config.ts
@@ -1,29 +1,29 @@
 import { defineConfig } from '@rsbuild/core';
-import {pluginVue } from '@rsbuild/plugin-vue';
-import { pluginLess} from "@rsbuild/plugin-less";
+import { pluginVue } from '@rsbuild/plugin-vue';
+import { pluginLess } from "@rsbuild/plugin-less";
 
 export default defineConfig({
-    html: {
-      template: './index.html',
+  html: {
+    template: './index.html',
+  },
+  source: {
+    entry: {
+      index: './src/main.ts',
     },
-    source: {
-      entry: {
-        index: './src/main.ts',
+  },
+  plugins: [
+    pluginVue(),
+    pluginLess(),
+  ],
+  server: {
+    host: '0.0.0.0',
+    port: 9002,
+    open: false,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080/',
+        changeOrigin: true,
       },
     },
-    plugins: [
-      pluginVue(),
-      pluginLess(),
-    ],
-    server: {
-      host: '0.0.0.0',
-      port: 9002,
-      open: false,
-      proxy: {
-        '/api': {
-          target: 'http://localhost:8080/',
-          changeOrigin: true,
-        },
-      },
-    },
+  },
 });
