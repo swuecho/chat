3431c4030af65bc16983d2e1bf7de5f0ebc3bd47
Hao Wu <echowuhao@gmail.com>
2025-03-01 17:25:25 +0800

    more

--- web/src/views/chat/layout/sider/index.vue
+++ web/src/views/chat/layout/sider/index.vue
@@ -111,7 +111,7 @@ function openAllSnapshot() {
                   @click="openAllSnapshot"
                 >
                   <template #icon>
-                    <SvgIcon icon="ri:file-copy-line" />
+                    <SvgIcon icon="ri:file-list-line" />
                   </template>
                 </NButton>
               </template>
@@ -125,7 +125,7 @@ function openAllSnapshot() {
                   @click="openBotAll"
                 >
                   <template #icon>
-                    <SvgIcon icon="ri:user-robot-line" />
+                    <SvgIcon icon="majesticons:robot-line" />
                   </template>
                 </NButton>
               </template>
@@ -139,7 +139,7 @@ function openAllSnapshot() {
                   @click="show = true"
                 >
                   <template #icon>
-                    <SvgIcon icon="ri:file-list-line" />
+                    <SvgIcon icon="ri:lightbulb-line" />
                   </template>
                 </NButton>
               </template>
