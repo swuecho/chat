a6af2201a2a04d73a02cc3729d6a96abccb10cbe
Hao Wu <echowuhao@gmail.com>
2025-03-02 09:18:39 +0800

    refactor: Replace SVG with SvgIcon component in bot view

--- web/src/views/bot/all.vue
+++ web/src/views/bot/all.vue
@@ -107,11 +107,7 @@ function postUrl(uuid: string): string {
     <header
       class="flex items-center justify-between h-16 z-30 border-b dark:border-neutral-800 bg-white/80 dark:bg-black/20 dark:text-white backdrop-blur">
       <div class="flex items-center ml-10">
-        <svg class="w-8 h-8 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-            d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-        </svg>
+        <SvgIcon icon="majesticons:robot-line" />
         <h1 class="text-xl font-semibold text-gray-900">
           {{ $t('bot.all.title') }}
         </h1>
