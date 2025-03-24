4259f849dfd8f4999a1d4261d00721256117ec0c
Hao Wu <echowuhao@gmail.com>
2025-03-24 22:24:34 +0800

    "Change API port from 8070 to 8080"

--- web/rsbuild.config.ts
+++ web/rsbuild.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
     open: false,
     proxy: {
       '/api': {
-        target: 'http://localhost:8070/',
+        target: 'http://localhost:8080/',
         changeOrigin: true,
       },
     },
