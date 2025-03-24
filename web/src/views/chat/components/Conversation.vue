c18fee97b0fabedb9b6e72d2ee49868deb9d1e64
Hao Wu <wuhaoecho@gmail.com>
2025-03-24 22:33:18 +0800

    Add chat only if not new (#654)
    * refactor: Remove unused deleteChat import in Conversation.vue
    
    * fix: only create new chat when dataSources has messages
    
    * feat: warn when creating new chat from empty conversation
    
    * Change API port to 8070 and update locales
    
    - Update API port from 8080 to 8070 in main.go and rsbuild.config.ts
    - Add "alreadyInNewChat" locale entries in en-US and zh-CN
    
    * "Change API port from 8070 to 8080"

--- web/src/views/chat/components/Conversation.vue
+++ web/src/views/chat/components/Conversation.vue
@@ -41,7 +41,7 @@ const { sessionUuid } = defineProps({
 
 
 const { isMobile } = useBasicLayout()
-const { addChat, deleteChat, updateChat, updateChatPartial } = useChat()
+const { addChat, updateChat, updateChatPartial } = useChat()
 const { scrollRef, scrollToBottom, scrollToBottomIfAtBottom } = useScroll()
 // session uuid
 chatStore.syncChatMessages(sessionUuid)
@@ -60,22 +60,15 @@ const appStore = useAppStore()
 
 
 async function handleAdd() {
-  const new_chat_text = t('chat.new')
-  //   //
-  //   // {
-  //   "uuid": "ff511942-43b4-4ebd-9fe8-f2ca405605ac",
-  //   "isEdit": false,
-  //   "title": "try improve code and",
-  //   "maxLength": 10,
-  //   "temperature": 1,
-  //   "topP": 1,
-  //   "maxTokens": 512,
-  //   "debug": false
-  // }//
-  const default_model_parameters = await getChatSessionDefault(new_chat_text)
-  await chatStore.addChatSession(default_model_parameters)
-  if (isMobile.value)
-    appStore.setSiderCollapsed(true)
+  if (dataSources.value.length > 0) {
+    const new_chat_text = t('chat.new')
+    const default_model_parameters = await getChatSessionDefault(new_chat_text)
+    await chatStore.addChatSession(default_model_parameters)
+    if (isMobile.value)
+      appStore.setSiderCollapsed(true)
+  } else {
+    nui_msg.warning(t('chat.alreadyInNewChat'))
+  }
 }
 
 // 添加PromptStore
