824766e586f326f952574ea2e5c244f3fd063b70
Hao Wu <wuhaoecho@gmail.com>
2025-07-27 10:02:30 +0800

    Add workspace_id to chat session query

--- e2e/lib/db/chat_session/index.ts
+++ e2e/lib/db/chat_session/index.ts
@@ -1,6 +1,6 @@
 export async function selectChatSessionByUserId(pool, userId: number) {
         const query = {
-                text: 'SELECT id, uuid, topic, created_at, updated_at, active, max_length, temperature, n, debug, model FROM chat_session WHERE user_id = $1 order by id',
+                text: 'SELECT id, uuid, topic, created_at, updated_at, active, max_length, temperature, n, debug, model, workspace_id FROM chat_session WHERE user_id = $1 order by id',
                 values: [userId],
         };
 
